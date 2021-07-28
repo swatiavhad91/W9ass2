9e78b909b982131eea9979d46d34f93c5ced8ed5
Swati Avhad <swatiavhad91@gmail.com>
2021-07-28 16:34:18 +0530

    W9ass2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import Home from "./home/Home";
+import UsersWithReduxSaga from "./redux-saga/UsersWithReduxSaga";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+            <div>
+              <Link to='/'>Home</Link>
+
+              |
+              <Link to='/redux-saga'>Redux-Saga</Link>
+
+            </div>
+            <Switch>
+
+              <Route path="/redux-saga" component={UsersWithReduxSaga} />
+
+              <Route path="/" component={Home} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/store/appReducers.js
@@ -0,0 +1,10 @@
+import {default as reduxSaga} from "./../redux-saga/reducer"
+import {combineReducers} from "redux";
+
+
+const reducers = combineReducers({
+
+    reduxSaga
+});
+
+export default reducers;
